fd1a4464043b877ae32ee8a9fd596c4c5a3f7d99
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-06-21 14:26:19 +0900

    卒業制作かどうかのチェック項目を追加

--- db/schema.rb
+++ db/schema.rb
@@ -466,6 +466,7 @@ ActiveRecord::Schema.define(version: 2021_08_28_145120) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id", "title"], name: "index_works_on_user_id_and_title", unique: true
+    t.boolean "graduation_work", default: false, null: false
     t.index ["user_id"], name: "index_works_on_user_id"
   end
 
